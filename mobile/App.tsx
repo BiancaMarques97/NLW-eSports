0e6dec8bb5c1f32fba8f4a21becb6aa930048a09
Bianca Marques <bianca.marques@1uptech.com.br>
2022-09-23 23:56:33 -0300

    NLW eSports

---
+++ mobile/App.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Background } from './src/components/Background';
+import { StatusBar } from 'react-native';
+import { Routes } from './src/routes';
+import { Loading } from './src/components/Loading';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+import React from 'react';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  return (
+      <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+       { fontsLoaded ? <Routes /> : <Loading />}
+    </Background>
+  );
+}
+
+
---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,16 @@
+export interface GameParams {
+    id: string;
+    title: string;
+    bannerUrl: string;
+  }
+
+declare global {
+    namespace ReactNavigation {
+        interface RootParamList{
+            home: undefined;
+            game: GameParams
+        }
+    }
+}
+
+export default global;
